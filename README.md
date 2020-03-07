615b09853d917bb1584e8b5bae70f0f3f61993b3
dv258 <60153503+dv258@users.noreply.github.com>
2020-03-07 16:14:05 -0500

    initial commit, skeleton setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Statistics Calculator - Team Project
+[![Build Status](https://travis-ci.org/dv258/is218-stats-calculator-team.svg?branch=master)](https://travis-ci.org/dv258/is218-stats-calculator-team)
---
+++ tests/tests_calculator.py
@@ -0,0 +1,10 @@
+import unittest
+
+from calculator.calculator import Calculator
+
+class TestCases(unittest.TestCase):
+	def setUp(self):
+		self.calculator = Calculator()
+
+	def test_instantiation(self):
+		self.assertIsInstance(self.calculator, Calculator)
