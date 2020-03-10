fd11bbdb410dc187dd8f82c72b4f9cdf186f6cb7
nartin9 <31773564+nartin9@users.noreply.github.com>
2020-03-10 14:33:00 -0400

    Update randomgenerator.py

--- randomgenerator/randomgenerator.py
+++ randomgenerator/randomgenerator.py
@@ -1,3 +1,31 @@
+import random
+
 class RandomGenerator:
 	def __init__(self):
 		pass
+	
+def ranInt(x,y):
+    return random.randint(x, y)
+
+def ranDec(x,y):
+    return random.uniform(a,b)
+
+def randIntSeed(x, y, z)
+    random.seed(z)
+    return random.randint(x, y)
+
+def randIntSeed(x, y, z)
+    random.seed(z)
+    return random.uniform(x, y)
+def randList(x, y, z)
+    nums = []
+    for i in range(0, z):
+        nums.append(random.randint(x, y))
+    return nums
+
+def randElement(elements)
+    return random.choice(elements)
+
+def randElements(elements, n)
+    values = []
+    for x in range(n):
