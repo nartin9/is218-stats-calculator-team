228d0f1adaef76004617bea2ae457e8975091799
dv258 <60153503+dv258@users.noreply.github.com>
2020-03-12 14:23:03 -0400

    add descstats methods for correlation

--- tests/tests_descriptivestats.py
+++ tests/tests_descriptivestats.py
@@ -31,9 +31,25 @@ class TestCases(unittest.TestCase):
 		data = [22, 56, 12, 32, 65, 15]
 		self.assertAlmostEqual(22.0786473, DescriptiveStatistics.stdev(data))
 
+	def test_pstdev(self):
+		data = [22, 56, 12, 32, 65, 15]
+		self.assertAlmostEqual(20.15495527, DescriptiveStatistics.pstdev(data))
+
 	def test_variance(self):
 		data = [22, 56, 12, 32, 65, 15]
 		self.assertAlmostEqual(487.46666666, DescriptiveStatistics.variance(data))
+
+	def test_covariance(self):
+		data1 = [14.2, 16.4, 11.9, 15.2, 18.5]
+		data2 = [215, 325, 185, 332, 406]
+
+		self.assertEqual(211.445, DescriptiveStatistics.covariance(data1, data2))
+
+	def test_pcovariance(self):
+		data1 = [14.2, 16.4, 11.9, 15.2, 18.5]
+		data2 = [215, 325, 185, 332, 406]
+
+		self.assertEqual(169.156, DescriptiveStatistics.pcovariance(data1, data2))
 
 	def test_quartiles_odd(self):
 		data = [9, 8, 2, 5, 2, 1, 12]
@@ -56,3 +72,15 @@ class TestCases(unittest.TestCase):
 	def test_meanDeviation(self):
 		data = [3, 2, 6, 1, 8]
 		self.assertEqual(2.4, DescriptiveStatistics.meanDeviation(data))
+
+	def test_sample_correlation(self):
+		data1 = [14.2, 16.4, 11.9, 15.2, 18.5]
+		data2 = [215, 325, 185, 332, 406]
+
+		self.assertAlmostEqual(0.94532854, DescriptiveStatistics.sampleCorrelation(data1, data2))
+
+	def test_population_correlation(self):
+		data1 = [14.2, 16.4, 11.9, 15.2, 18.5]
+		data2 = [215, 325, 185, 332, 406]
+
+		self.assertAlmostEqual(0.94532854, DescriptiveStatistics.populationCorrelation(data1, data2))
