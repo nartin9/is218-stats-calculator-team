09c728b2626cecd0f68eddb9a6526055abd6514a
nartin9 <31773564+nartin9@users.noreply.github.com>
2020-03-12 21:44:26 -0400

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -10,31 +10,31 @@ from mathoperations.logarithm import Logarithm
 class Calculator:
 	def __init__(self):
 		self.result = None
-
+	# result = x+y
 	def addition(self, x, y=None):
 		self.result = Addition.sum(x, y)
 		return self.result
-		
+	#result= x-y	
 	def subtraction(self, x, y):
 		self.result = Subtraction.difference(x, y)
 		return self.result
-		
+	#result= x*y
 	def multiplication(self, x, y):
 		self.result = Multiplication.product(x, y)
 		return self.result
-		
+	#result= x/y
 	def division(self, x, y):
 		self.result = Division.quotient(x, y)
 		return self.result
-		
+	# result = x^y	
 	def exponentiation(self, x, y):
 		self.result = Exponentiation.power(x, y)
 		return self.result
-		
+	# result= x to the y root
 	def nthroot(self, x, y):
 		self.result = NthRoot.root(x, y)
 		return self.result
-		
+	# result = logarithm base x of y	
 	def logarithm(self, x, y):
 		self.result = Logarithm.logarithm(x, y)
 		return self.result
